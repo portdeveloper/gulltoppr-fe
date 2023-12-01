5ea69d8d26d9d8d53792c25890a2d46e280b1af0
port <108868128+portdeveloper@users.noreply.github.com>
2023-12-02 00:44:11 +0300

    Update backend address

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
   const getAbi = async () => {
     setIsLoading(true);
     try {
-      const response = await fetch(`http://localhost:8080/generate-abi/${contractAddress}`);
+      const response = await fetch(`https://heimdall-api.fly.dev/generate-abi/${contractAddress}`);
       if (!response.ok) {
         throw new Error("Network response was not ok.");
       }
